42ae52809a392a55943c77218020b3c5825bd126
Justin Toman <justintoman@gmail.com>
2025-04-22 16:52:07 -0500

    Update packages

--- app/routes/create/route.tsx
+++ app/routes/create/route.tsx
@@ -18,6 +18,7 @@ export async function loader({ request }: Route.LoaderArgs) {
   if (!user) {
     return redirect("/login");
   }
+  return null;
 }
 
 export async function action({ request }: Route.ActionArgs) {
